7f0f1fad6e8897dca221225e2c84568055a33144
simperiale <ssimperiale@gmail.com>
2025-06-11 13:30:13 -0300

    cache-subtipos

--- app/common/cache.py
+++ app/common/cache.py
@@ -12,6 +12,7 @@ CACHE_TIMEOUT_MEDIUM = 3600  # 1 hour
 CACHE_TIMEOUT_LONG = (3600 * 6 ) # 6 hours
 
 # Leer configuraciones de Redis desde variables de entorno
+
 redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
 redis_db = int(os.getenv('REDIS_DB', 0))
